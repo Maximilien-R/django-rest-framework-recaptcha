9f73135e5903062bea7b849211926eb5362408b9
Maximilien Raulic <raulic.maximilien@gmail.com>
2018-07-01 17:00:07 +0200

    chore(conf): override default prefix on AppConf

--- rest_framework_recaptcha/conf.py
+++ rest_framework_recaptcha/conf.py
@@ -3,10 +3,13 @@ from django.conf import settings  # noqa: F401
 from appconf import AppConf
 
 
-class DRFReCaptchaConf(AppConf):
+class DjangoRestFrameworkRecaptchaConf(AppConf):
     """
     Django REST framework reCAPTCHA settings.
     """
+
+    class Meta:
+        prefix = "drf_recaptcha"
 
     VERIFY_ENDPOINT = "https://www.google.com/recaptcha/api/siteverify"
 
